ebdc95407e77621e3cc3d5e63251d4252e4c680f
Arthur <arthurpvilaca@gmail.com>
2021-02-03 23:06:00 -0300

    aula 2 finalizada

--- js/main.js
+++ js/main.js
@@ -1,2 +1,29 @@
-var nome = "Arthur Vilaça"
-alert("Bem vindo" + nome);
+
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(fruta.cor);
+
+
+
+
+
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista.length);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+//var nome = "Arthur Vilaça"
+//alert("Bem vindo" + nome);
+//var nome = "Arthur Vilaça";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + "tem" + idade + "anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
