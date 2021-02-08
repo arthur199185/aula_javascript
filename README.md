38d34c2845a44cf9caa262720e7455054fc0ceba
Arthur <arthurpvilaca@gmail.com>
2021-02-08 20:11:07 -0300

    finalizada aula de introduçao

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # aula_javascript
 Introduçao ao javascript
-
+Aulas iniciando com funçoes, condicionais, dates, laços de repetição, arrays, dicionario, alertas, perguntas, console.log, lenght, upper e lower case, join, toString, reverse, criar botao na pagina, onclick, innerHTML, document.get , window.open, window.location.hret, onmouseclick, onmouse over e out para um ou varios elementos especificos, onloead, on change,select, option, .value.
+Finalizada Introduçao ao JVS
--- js/main.js
+++ js/main.js
@@ -1,12 +1,55 @@
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar!</b>"
+    //alert("Obrigado por clicar");
+}
+function redirecionar(){
+    window.open("https://youtube.com");
+    //window.location.href = "https://youtube.com";
+}
 
-var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse"
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse"
+    //alert("Trocar texto");
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Passe o mouse aqui"
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui"
+}
+
+function load(){
+    alert("Pagina carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
+/*function soma(n1, n2){
+    return n1 + n2;
+}
+var validar; // var global
+function validaIdade(idade){
+    var validar; // var local
+    if (idade >= 18){
+        return true
+    }else{
+        return false
+    }
+}
+
+var idade = prompt("Qual sua idade?")
+console.log(validaIdade(idade));
+alert(soma(5, 10));*/
+
+//var d = new Date();
+//while e for normal; if/else normal
+//var idade = prompt("Qual sua idade?") - exibe perguntas
+
+/*var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
 console.log(frutas);
-alert(fruta.cor);
-
-
-
-
-
+alert(fruta.cor);*/
 
 //var lista = ["maça", "pêra", "laranja"];
 //lista.push("uva");
